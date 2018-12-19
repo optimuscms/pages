ff9fda439d5c50e782c8f319115080e57a2785b9
Jack Robertson <jack@optixsolutions.co.uk>
2018-12-19 11:45:02 +0000

    Fixed meta keys

--- src/Http/Resources/PageResource.php
+++ src/Http/Resources/PageResource.php
@@ -20,7 +20,10 @@ class PageResource extends Resource
             'has_fixed_template' => $this->has_fixed_template,
             'contents' => ContentResource::collection($this->contents),
             'media' => MediaResource::collection($this->media),
-            'meta' => $this->getAllMeta(),
+            'meta' => [
+                'title' => $this->getMeta('title'),
+                'description' => $this->getMeta('description')
+            ],
             'children_count' => $this->when(
                 ! is_null($this->children_count),
                 $this->children_count
